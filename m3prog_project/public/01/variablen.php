83868a53d919bb9dc350a83ce9c0ef4347e27c10
Fred <36563@ma-web.nl>
2024-03-01 10:19:46 +0100

    les 1 opdracht 4 gemaakt

---
+++ m3prog_project/public/01/variablen.php
@@ -0,0 +1,24 @@
+<?php
+$naam = "Fred";
+$woonplaats = "Amstelveen";
+$leeftijd = 17;
+$temperatuur = 26.6;
+
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    echo "mijn naam is $naam, ik woon in $woonplaats, $leeftijd en de tempratuur is $temperatuur graden"
+    ?>
+    
+</body>
+</html>
